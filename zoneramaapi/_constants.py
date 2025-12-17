f7b86c5a998c5ee29c943c74c2d1e97b58050459
Jan Mach <machous02@gmail.com>
2025-12-18 00:41:54 +0200

    download album functionality

--- zoneramaapi/_constants.py
+++ zoneramaapi/_constants.py
@@ -4,3 +4,7 @@
 
 APISERVICE_WSDL_URL = "https://zonerama.com/services/zpsforandroid/apiservice.asmx?WSDL"
 DATASERVICE_WSDL_URL = "https://zonerama.com/services/zpsforandroid/dataservice.asmx?WSDL"
+
+ZIP_REQUEST_URL = "https://zonerama.com/Zip/Album"
+ZIP_READY_URL = "https://zonerama.com/Zip/IsReady"
+ZIP_DOWNLOAD_URL = "https://zonerama.com/Zip/Download"
